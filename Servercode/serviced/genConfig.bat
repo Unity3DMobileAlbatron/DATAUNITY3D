ff4850c2c414e4282bbb34eab0f5387900b6030c
Tienguyenviettruong <35688265+Tienguyenviettruong@users.noreply.github.com>
2018-09-18 23:42:03 +0700

    Add files via upload

---
+++ Servercode/serviced/genConfig.bat
@@ -0,0 +1,2 @@
+java -jar ../../../../luxianres/branches/test826/code/lib/config.jar -configxml ../../../../luxianres/branches/test826/csv/cfg.xml -outputencoding UTF8 -lan java -codedir externs -datadir config -group server
+pause
